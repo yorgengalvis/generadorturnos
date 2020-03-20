ee75e7f04ce14abc1549264440de0c1c2ff9c993
YorgenGalvis <YorgenGalvis@192.168.0.14>
2020-03-20 13:24:28 -0500

    Implementación de asignaciones de turnos

---
+++ src/Mundo/AsignacionesTurnos.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mundo;
+
+import Modelo.*;
+import Paquete.Cola;
+
+/**
+ *
+ * @author YorgenGalvis
+ */
+public class AsignacionesTurnos {
+    Cola<Turno> turnos;
+
+    public AsignacionesTurnos() {
+        turnos=new Cola<Turno>();
+    }
+    
+    public String obtenerTurno(int modulo,Persona asignar){
+        String msg=null;
+        
+        
+        return msg;
+    }
+    
+    
+   
+    
+    
+    
+}
